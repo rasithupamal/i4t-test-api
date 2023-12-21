59de8ea303d683a825cbcb1297b4ac66a9a17cac
Rasith Upamal <rasithupamal@gmail.com>
2023-12-21 11:12:59 +0530

    login function implemented.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,13 +6,14 @@ use App\Http\Requests\AuthLoginRequest;
 use App\Http\Requests\AuthRegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-   
+
     public function register(AuthRegisterRequest $request)
     {
         try {
@@ -31,6 +32,7 @@ class AuthController extends Controller
                 return response()->json([
                     'message' => 'Successfully registered',
                     'accessToken' => $token,
+                    'token_type' => 'Bearer',
                 ], 201);
             } else {
                 return response()->json(['error' => 'Please fill mandatory details'], 422);
@@ -38,9 +40,36 @@ class AuthController extends Controller
         } catch (ValidationException $e) {
             return response()->json(["message" => "Please fill all fields", 'error' => $e->validator->errors()], 400);
         } catch (\Exception $e) {
+            Log::error($e);
             return response()->json(['error' => 'Internal Server Error'], 500);
         }
+    }
 
+    public function login(AuthLoginRequest $request)
+    {
+        try {
+            $request->validated();
+            $credentials = request(['email', 'password']);
+            if (!Auth::attempt($credentials)) {
+                return response()->json([
+                    'message' => 'Unauthorized'
+                ], 401);
+            }
+
+            $user = $request->user();
+            $tokenResult = $user->createToken($request->email);
+            $token = $tokenResult->plainTextToken;
+
+            return response()->json([
+                'accessToken' => $token,
+                'token_type' => 'Bearer',
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json(["message" => "Please fill all fields", 'error' => $e->validator->errors()], 400);
+        } catch (\Exception $e) {
+            Log::error($e);
+            return response()->json(['error' => 'Internal Server Error'], 500);
+        }
     }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/login', [AuthController::class, "login"])->name("auth.login");
 Route::post('/register', [AuthController::class, "register"])->name("auth.register");
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
